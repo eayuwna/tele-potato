22e86ca80ffc393e26ac0b5364095b8a23571fde
eayuwna <121429604+eayuwna@users.noreply.github.com>
2023-03-13 16:44:19 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    Alien = receivedNumber
+})
+input.onButtonPressed(Button.AB, function () {
+    Alien = randint(10, 20)
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendNumber(Alien)
+    Alien = -1
+})
+let Alien = 0
+radio.setGroup(1)
+Alien = -1
+basic.forever(function () {
+    if (Alien == 0) {
+    	
+    }
+    if (Alien < 0) {
+    	
+    }
+    if (Alien > 0) {
+        basic.showIcon(IconNames.Ghost)
+        Alien += -1
+    }
+})
