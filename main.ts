b0752d45410b521132b9171ff945db889735bc2e
eayuwna <121429604+eayuwna@users.noreply.github.com>
2023-03-15 15:09:23 +0000

    Added customisations

--- main.ts
+++ main.ts
@@ -14,12 +14,16 @@ Alien = -1
 basic.forever(function () {
     if (Alien == 0) {
         basic.showIcon(IconNames.Skull)
+        music.playMelody("G F E D D C - - ", 240)
     }
     if (Alien < 0) {
         basic.clearScreen()
     }
     if (Alien > 0) {
         basic.showIcon(IconNames.Ghost)
+        music.playTone(587, music.beat(BeatFraction.Half))
+        basic.showIcon(IconNames.House)
+        music.playTone(494, music.beat(BeatFraction.Whole))
         Alien += -1
     }
 })
